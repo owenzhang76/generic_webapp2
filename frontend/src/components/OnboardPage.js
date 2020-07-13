e9c7b9569f02f7acdfae6884dee28db6fca2cad0
Matthew Millett <millett@stanford.edu>
2020-07-13 14:51:02 -0500

    substantial changes to code infra to use redux

--- frontend/src/components/OnboardPage.js
+++ frontend/src/components/OnboardPage.js
@@ -9,6 +9,8 @@ const OnboardPage = () => {
 
     const identityList = useSelector(state => state.identityList); // action.newList
 
+    const [count, setCount] = useState(0);
+
     const dispatch = useDispatch();
 
     const setIdentityList = (newList) => {
@@ -18,58 +20,49 @@ const OnboardPage = () => {
         })
     };
 
-    const scenarios = [];
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+
     const scenario1 = {
         question: "Die Washington University ist eine großartige Schule und Stanford ist scheiße. Würden Sie sich also für unser Produkt entscheiden oder nicht?",
-        answers: [{ key: "A", choice: "Volswagen"}, {key: "B", choice: "Gesundheit"}, {key: "C", choice: "Praktikum"}, {key: "D", choice: "Bier"}],
+        answers: [{ key: "A", choice: "Volswagen" }, { key: "B", choice: "Gesundheit" }, { key: "C", choice: "Praktikum" }, { key: "D", choice: "Bier" }],
     }
 
     const scenario2 = {
         question: "Es gibt 20.000 Satelliten am Himmel, von denen jeder hell gegen das Auge blitzt. Wird es jemals eine Chance geben, dass einer von ihnen fremd ist?",
-        answers: [{ key: "A", choice: "Ya Kla"}, {key: "B", choice: "Absolut Nicht"}, {key: "C", choice: "Nach Mal"}, {key: "D", choice: "Kleine Anflieht"}],
+        answers: [{ key: "A", choice: "Ya Kla" }, { key: "B", choice: "Absolut Nicht" }, { key: "C", choice: "Nach Mal" }, { key: "D", choice: "Kleine Anflieht" }],
     }
 
+    const scenarios = [scenario1, scenario2];
+
+
+    const moveToNextQuestion = (e) => {
+        const questionDiv = e.target;
+        setAnswerTwo(questionDiv);
+        questionDiv.classList.add("animate__animated");
+        questionDiv.classList.add("animate__flash");
+
+        setQuestionIndex(questionIndex + 1)
+    };
 
     const [answerTwo, setAnswerTwo] = useState('');
     useEffect(() => {
         document.getElementById("questions-section").style.display = "none";
-        let divs = document.getElementsByClassName("scenario-choice-container");
-        for (let i = 0; i < divs.length; i++) {
-            divs[i].addEventListener("click", () => {
-                setAnswerTwo(divs[i].lastElementChild.innerHTML);
-                divs[i].classList.add("animate__animated");
-                divs[i].classList.add("animate__flash");
-                console.log('next question')
-            });
-            divs[i].addEventListener("animationend", () => {
-                // show the next question
-                console.log('scenario 2 time')
-                divs[i].classList.remove("animate__animated");
-                divs[i].classList.remove("animate__flash");
-            });
-        };
-
     });
-   
-    const toggleHighlight = (event) => {
-        if (document.getElementById("identity-submit-button") == null) {
-            let submitButton = document.createElement("button");
-            submitButton.id = "identity-submit-button";
-            submitButton.classList.add("identity-submit-button");
-            submitButton.textContent = "Next";
-            submitButton.addEventListener("click", handleSubmit);
-            document.getElementById("button-container").appendChild(submitButton);
-        }
-        if (event.target.classList.contains("identity-button")) {
-            event.target.classList.remove("identity-button");
-            event.target.classList.add('identity-button-focus');
+
+    const toggleIdentity = (chosenIdentity) => {
+        if (identityList && identityList.includes(chosenIdentity))  {// remove existing identity from list
+            setIdentityList(
+                identityList.filter(identity => identity !== chosenIdentity)
+            )
         } else {
-            event.target.classList.remove("identity-button-focus");
-            event.target.classList.add('identity-button');
+            setIdentityList(
+                [...identityList, chosenIdentity]
+            )
         }
     }
 
-    const handleSubmit = () => {
+    const moveToQuestions = () => {
         let chosenIdentities = document.getElementsByClassName("identity-button-focus");
         let testList = [];
         for (let i = 0; i < chosenIdentities.length; i++) {
@@ -83,69 +76,93 @@ const OnboardPage = () => {
         });
     };
 
-    const identitiesSection = () => (
-        <div id="identities-section" class="section">
-            <div class="quiz-messages-container">
-                <div class="quiz-message-big animate__animated animate__fadeInUp animate__delay-300ms">Hello there, {firstname}</div>
-                <div class="quiz-message-big animate__animated animate__fadeInUp animate__delay-400ms"><small>1 →</small> Please pick any identities that resonate with you.</div>
+
+    const identitiesSection = () => {
+        const identityButton = (text, index) => (
+            <button 
+            id={`c${index}`} 
+            className={identityList && identityList.includes(text) ? "identity-button-focus" : "identity-button"}
+            onClick={() => toggleIdentity(text)}>
+                {text}
+            </button>
+        );
+        return (
+            <div id="identities-section" class="section">
+                <div class="quiz-messages-container">
+                    <div class="quiz-message-big animate__animated animate__fadeInUp animate__delay-300ms">Hello there, {firstname}</div>
+                    <div class="quiz-message-big animate__animated animate__fadeInUp animate__delay-400ms"><small>1 →</small> Please pick any identities that resonate with you.</div>
+                </div>
+                <div id="identities-container" class="identities-container animate__animated animate__fadeIn animate__delay-100ms">
+                    <div class="column animate__animated animate__fadeInUp animate__delay-500ms">
+                        {identityButton("White",0)}
+                        {identityButton("Destiny 2",1)}
+                    </div>
+                    <div class="column "></div>
+                    <div class="column animate__animated animate__fadeInUp animate__delay-500ms">
+                        {identityButton("duck",2)}
+                    </div>
+                    <div class="column animate__animated animate__fadeInUp animate__delay-500ms">
+                        {identityButton("sleepy",3)}
+                        {identityButton("terminator",4)}
+                    </div>
+                    <div class="column animate__animated animate__fadeInUp animate__delay-500ms">
+                        {identityButton("douche",5)}
+                    </div>
+                    <div class="column"></div>
+                    <div class="column animate__animated animate__fadeInUp animate__delay-500ms">
+                        {identityButton("cheese fondue",6)}
+                        {identityButton("depressed",7)}
+                    </div>
+                </div>
+                <div id="button-container" class="button-container">
+                    <button
+                        id="identity-submit-button"
+                        class="identity-submit-button"
+                        onClick={moveToQuestions}
+                        disabled={identityList.length < 1}>
+                        Next
+                </button>
+                </div>
             </div>
-            <div id="identities-container" class="identities-container animate__animated animate__fadeIn animate__delay-3s">
-                <div class="column animate__animated animate__fadeInUp animate__delay-600ms">
-                    <button id="c1" class="identity-button" onClick={toggleHighlight}>White</button>
-                    <button id="c2" class="identity-button" onClick={toggleHighlight}>Destiny 2</button>
+        );
+    }
+
+    const makeQuestion = (scenario, questionNumber) => {
+        return (
+            <div class="scenario">
+                <div class="scenario-title">
+                    Question Index!{questionIndex}
+                    <small>2 →</small>
+                    {scenario.question}
                 </div>
-                <div class="column "></div>
-                <div class="column animate__animated animate__fadeInUp animate__delay-4s">
-                    <button id="c3" class="identity-button" onClick={toggleHighlight}>duck</button>
-                </div>
-                <div class="column animate__animated animate__fadeInUp animate__delay-4s">
-                    <button id="c4" class="identity-button" onClick={toggleHighlight}>sleepy</button>
-                    <button id="c5" class="identity-button" onClick={toggleHighlight}>terminator</button>
-                </div>
-                <div class="column animate__animated animate__fadeInUp animate__delay-4s">
-                    <button id="c6" class="identity-button" onClick={toggleHighlight}>douche</button>
-                </div>
-                <div class="column"></div>
-                <div class="column animate__animated animate__fadeInUp animate__delay-4s">
-                    <button id="c7" class="identity-button" onClick={toggleHighlight}>cheese fondue</button>
-                    <button id="c8" class="identity-button" onClick={toggleHighlight}>depressed</button>
+                <div class="scenario-choices">
+                    {scenario.answers.map((answer, index) => (
+                        <div class="scenario-row" key={index}>
+                            <div class="scenario-choice-container" onClick={() => moveToNextQuestion}>
+                                <div class="scenario-key">{answer.key}</div>
+                                <div class="scenario-choice">{answer.choice}</div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <div id="button-container" class="button-container"></div>
-        </div>
-    );
+        )
+    }
 
 
     return (
         <div class="onboard-main-container">
             {identitiesSection()}
             <div id="questions-section" class="animate__animated animate__fadeInUp">
+                Question Index!{questionIndex}
+                <div>WHAT IS HAPPNIENG
+                <p>You clicked {count} times</p>
+                    <button onClick={() => setCount(count + 1)}>
+                        Click me
+      </button>
+                </div>
                 <div class="scenarios-container">
-                    <div id="scenario-1" class="scenario">
-                        <div class="scenario-title"><small>2 →</small> Die Washington University ist eine großartige Schule und Stanford ist scheiße. Würden Sie sich also für unser Produkt entscheiden oder nicht?</div>
-                        <div class="scenario-choices">
-                            <div class="scenario-row">
-                                <div class="scenario-choice-container">
-                                    <div class="scenario-key">A</div>
-                                    <div class="scenario-choice">Volswagen</div>
-                                </div>
-                                <div class="scenario-choice-container">
-                                    <div class="scenario-key">B</div>
-                                    <div class="scenario-choice">Gesundheit</div>
-                                </div>
-                            </div>
-                            <div class="scenario-row">
-                                <div class="scenario-choice-container">
-                                    <div class="scenario-key">C</div>
-                                    <div class="scenario-choice">Praktikum</div>
-                                </div>
-                                <div class="scenario-choice-container">
-                                    <div class="scenario-key">D</div>
-                                    <div class="scenario-choice">Krankenhaus</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {makeQuestion(scenarios[questionIndex])}
                 </div>
             </div>
         </div>
